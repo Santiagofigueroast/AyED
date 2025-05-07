37bac39f8c171ba3e2289911e18c230e91ae97fe
Santiago Figueroa <santiagofigueroasf2006@gmail.com>
2025-05-07 20:21:15 -0300

    Add files via upload

---
+++ main.cpp.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string nombre,apellido;
+    cout<<"Cual es su nombre?"<<endl;
+    cin>>nombre;
+    cout<<"Cual es su apellido?"<<endl;
+    cin>>apellido;
+    cout<<"Hola "<< nombre << " " << apellido <<endl;
+    return 0;
+}
